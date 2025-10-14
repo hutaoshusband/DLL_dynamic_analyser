ef71f2e171d0a8811dcaf9a06f8cb79c4fdf6edf
HUTAOSHUSBAND <hutaoshusband@zohomail.eu>
2025-10-14 21:55:43 +0200

    Delete cs2_creator directory

--- cs2_creator/build.rs
+++
@@ -1,9 +0,0 @@
-extern crate embed_resource;
-
-fn main() {
-    // Diese Zeile sorgt dafür, dass das build-Skript neu ausgeführt wird, wenn sich das Manifest oder das RC-Skript ändert.
-    println!("cargo:rerun-if-changed=cs2_creator.manifest");
-    println!("cargo:rerun-if-changed=cs2_creator.rc");
-    // Diese Zeile kompiliert das RC-Skript, welches das Manifest einbindet.
-    embed_resource::compile("cs2_creator.rc", embed_resource::NONE);
-}
